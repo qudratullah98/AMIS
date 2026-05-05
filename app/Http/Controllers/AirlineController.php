4cf916fbd98d950ed837091cf5e3a2c01a2ebf6d
Asadullah omar <113917560+Asadullahomar@users.noreply.github.com>
2026-05-05 14:04:37 +0430

    done airline

--- app/Http/Controllers/AirlineController.php
+++ app/Http/Controllers/AirlineController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 use App\Models\AircraftType;
-=======
->>>>>>> ecfe4ff1eb063c8d12fc42777753752492068f28
 use App\Models\Airline;
 use Inertia\Inertia;
 
@@ -17,7 +14,6 @@ class AirlineController extends Controller
     public function index()
     {
         $search   = request()->input('query');
-<<<<<<< HEAD
         $perPage  = request()->input('perPage', 13);
 
         $airlines  = Airline::when($search, function ($query, $search) {
@@ -42,16 +38,4 @@ class AirlineController extends Controller
 
     }
 
-=======
-        $perPage  = request()->input('perPage', 10);
-
-
-        $airLines = Airline::with(['flytes', 'sghaServices'])->whereUserAirport()->when($search, function ($query, $search) {
-            return $query->search($search);
-        })->latest()->paginate($perPage);
-        return Inertia::render('Airlines/Index', [
-            'airLines' => $airLines,
-        ]);
-    }
->>>>>>> ecfe4ff1eb063c8d12fc42777753752492068f28
 }
